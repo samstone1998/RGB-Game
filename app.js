ae45ec2ca122e011b9d41a820b29c3175f10567a
samstone1998 <42448549+samstone1998@users.noreply.github.com>
2020-01-25 21:39:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 
 
-let answer1;
-let answer2;
-let answer3;
-let answer4;
+
+let answer;
+let score = 0;
+let question;
+const check_answer = event => {if(event.target.id === answer){
+  score +=1;
+  document.getElementById("score_num").innerHTML = score;
+}
+else{
+  score = 0;
+  document.getElementById("score_num").innerHTML = 0;
+}
+generate_challenge()};
+
 
 window.onload = function(){
   generate_challenge();
-answer1 = document.getElementById("answer-1");
-answer2 = document.getElementById("answer-2");
-answer3 = document.getElementById("answer-3");
-answer4 = document.getElementById("answer-4");
+  document.getElementById('answer-1').addEventListener('click',check_answer);
+  document.getElementById('answer-2').addEventListener('click',check_answer);
+  document.getElementById('answer-3').addEventListener('click',check_answer);
+  document.getElementById('answer-4').addEventListener('click',check_answer);
+
 }
 
 
 
 function generate_challenge(){
-  console.log("Page is ready!");
-}
-
-function check_answer(){
-  // will check the user clicked the correct answer
+  let colors = [];
+  for(let i = 1; i < 5;i++){
+    let red = Math.round(Math.random() * 255);
+    let green = Math.round(Math.random() * 255);
+    let blue = Math.round(Math.random() * 255);
+    document.getElementById(`answer-${i}`).style.backgroundColor = `rgb(${red},${green},${blue})`;
+    colors.push(`rgb(${red},${green},${blue})`);
+  }
+  answer = `answer-${Math.floor(Math.random()*colors.length+1)}`;
+  document.getElementById('game-question').innerHTML = document.getElementById(answer).style.backgroundColor;
 }
