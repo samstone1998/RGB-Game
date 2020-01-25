a987a0d0215dfc26726def1dd2563d4500a4e06f
samstone1998 <42448549+samstone1998@users.noreply.github.com>
2020-01-25 13:24:49 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 
 
+let answer1;
+let answer2;
+let answer3;
+let answer4;
+
+window.onload = function(){
+  generate_challenge();
+answer1 = document.getElementById("answer-1");
+answer2 = document.getElementById("answer-2");
+answer3 = document.getElementById("answer-3");
+answer4 = document.getElementById("answer-4");
+}
+
+
 
 function generate_challenge(){
-  // will generate the question level
+  console.log("Page is ready!");
 }
 
 function check_answer(){
